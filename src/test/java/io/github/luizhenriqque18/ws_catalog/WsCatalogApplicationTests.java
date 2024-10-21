0b53b3706f26ec67dd2fa6156263c4c03c0edf4c
Luiz Souza <luizhenriqque15@gmail.com>
2024-10-21 14:09:47 -0400

    init project

---
+++ src/test/java/io/github/luizhenriqque18/ws_catalog/WsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.luizhenriqque18.ws_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
